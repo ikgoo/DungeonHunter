bb66a21f693ed0fb7641bf67b7d9ab52e28218ef
ikgoo <lhscj2466@gmail.com>
2016-11-10 23:39:11 +0900

    최초추가

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1 : MonoBehaviour {
+
+    public GameObject preKnight;
+
+	// Use this for initialization
+	void Start () {
+
+        GameObject k = Instantiate(preKnight);
+        k.transform.parent = transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
